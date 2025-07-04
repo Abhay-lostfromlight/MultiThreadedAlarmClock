081e4d2b8066ee6ff2a3885f3345c5ad493df5a9
Abhay-lostfromlight <abhaysharma8887@gmail.com>
2025-07-04 16:18:06 +0530

    Update AlarmClock.java and project files

---
+++ src/main/java/MultiThreadedAlarmClock/AlarmClock.java
@@ -0,0 +1,24 @@
+package MultiThreadedAlarmClock;
+ import java.time.LocalDateTime;
+public class AlarmClock {
+    public LocalDateTime getDateTime() {
+        return dateTime;
+    }
+
+    public void setDateTime(LocalDateTime dateTime) {
+        this.dateTime = dateTime;
+    }
+
+    public String getReminder() {
+        return reminder;
+    }
+
+    public void setReminder(String reminder) {
+        this.reminder = reminder;
+    }
+
+    private LocalDateTime dateTime;
+    private String reminder;
+
+
+}
